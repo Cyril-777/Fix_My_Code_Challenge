f7ff72aa7cac54f4301efb9beebcc64b0b81f826
Cyril-777 <kyrillosmaher10@gmail.com>
2023-05-28 06:55:05 -0700

    double underscores changed

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -38,9 +38,9 @@ class User():
         - Hash `pwd` in MD5 before assign to `__password`
         """
         if pwd is None or type(pwd) is not str:
-            self.password = None
+            self.__password = None
         else:
-            self.password = hashlib.md5(pwd.encode()).hexdigest().lower()
+            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
 
     def is_valid_password(self, pwd):
         """
@@ -54,7 +54,7 @@ class User():
             return False
         if self.password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().upper() == self.password
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.password
 
 
 if __name__ == '__main__':
